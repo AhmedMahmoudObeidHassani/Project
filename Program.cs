9c5357a440574434eee243fe036db2fba7a1cd4a
AhmedMahmoudObeidHassani <s1023081@uomustansiriyah.edu.iq>
2024-11-28 18:21:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Channels;
+
+namespace Project
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+    }
+}
